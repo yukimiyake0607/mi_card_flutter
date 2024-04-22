b5e65c07ab9d33c81f0c6ab3bbb24dacc732c2f9
yukimiyake0607 <yukimiyake38@gmail.com>
2024-04-22 16:38:25 +0900

    プロフィール追加

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,65 @@ class MyApp extends StatelessWidget {
               Text(
                 'Yuki Miyake',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 50,
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSans3',
+                    color: Colors.teal.shade100,
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+81 090 1234 5678',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontFamily: 'SourceSans3',
+                          color: Colors.teal[900]),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'yukimiyake38@gmail.com',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal[900],
+                          fontFamily: 'SourceSans3'),
+                    ),
+                  ],
                 ),
               ),
             ],
